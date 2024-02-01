cf0c6dbc824063192d42d6be85af87a2556ddd1c
Kaslariya-Mrugen <mrugenk30@gmail.com>
2024-02-01 20:36:38 +0530

    First

---
+++ CPP-1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"THIS IS FIRST PROGRAM OF CPP\n---------------------------------"<<endl;
+    int a;
+
+    cout<<"Enter Any Number ";
+    cin>>a;
+
+    cout<<"Your Number Is : "<<a;
+
+    return 0;
+}
